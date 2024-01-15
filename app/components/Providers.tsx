7b278a9e8fc3cea152338a0ad7c46f40ed0acb76
Asad Raza <143411340+asadrazanaqv@users.noreply.github.com>
2024-01-16 04:33:57 +0500

    upd

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="https://fashion-destination-8tcdjjlg6-asad-razas-projects.vercel.app/stripe/success"
-      cancelUrl="https://fashion-destination-8tcdjjlg6-asad-razas-projects.vercel.app/stripe/error"
+      successUrl="https://fashion-destination.vercel.app/stripe/success"
+      cancelUrl="https://fashion-destination.vercel.app/stripe/error"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
