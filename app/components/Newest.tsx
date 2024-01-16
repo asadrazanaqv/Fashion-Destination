dbc5bf1bb0ca8f144b5f4d87586ceaede92e8ed7
Asad Raza <143411340+asadrazanaqv@users.noreply.github.com>
2024-01-16 05:32:50 +0500

    upd

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -18,12 +18,12 @@ async function getData() {
 
   return data;
 }
-export const dynamic = "force-dynamic";
 
 
 export default async function Newest() {
   const data: simplifiedProduct[] = await getData();
-
+  
+  const dynamic = "force-dynamic";
   return (
     <div>
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
